46af8ec8c8ca6a6cd5f49304202ca9dfc80603c9
Bob Yuan <bob.yuan@yahoo.com>
2023-09-05 15:08:02 -0700

    Add routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
+import { Routes, provideRouter } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { routes } from './app.routes';
+
+// export const routes : Routes = [
+//   { path: '**', redirectTo: 'home', pathMatch: 'full' },
+//   { path: 'home', component: HomeComponent},
+//   { path: 'about', component: AboutComponent},
+// ];
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    provideRouter(routes)
+  ]
 };
