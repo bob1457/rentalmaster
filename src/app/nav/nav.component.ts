2f9be00cb80964713ccebe0f54933a20655f07ba
Bob Yuan <bob.yuan@yahoo.com>
2023-11-09 22:18:21 -0800

    Config basic authentication with local emulator

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { UserInfo } from '../models/user-info';
 
 @Component({
   selector: 'app-nav',
@@ -13,13 +14,35 @@ import { environment } from 'src/environments/environment';
 export class NavComponent {
   showMenu = false;
   env = environment.production;
+  user: UserInfo | null;
+  providers = ['twitter', 'github', 'aad'];
+  redirect = '/manage'; // window.location.pathname;
   
   
   toggleNavbar(){
     this.showMenu = !this.showMenu;
   }
 
-  ngOnInit() {
-    console.log('object', this.env)
+  async ngOnInit() {
+
+    console.log('production', this.env)
+    console.log('redirect', this.redirect)
+    this.user = await this.getUserInfo();
+  }
+
+  async getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
+
+  logout() {
+    console.log('logout')
   }
 }
