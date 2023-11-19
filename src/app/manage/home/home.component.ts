5545c89ca51d3bda9fc8cd847127635710aef10a
Bob Yuan <bob.yuan@yahoo.com>
2023-11-19 15:41:03 -0800

    Initial style of top tiles on dashboard

--- src/app/manage/home/home.component.ts
+++ src/app/manage/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    FontAwesomeModule
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
