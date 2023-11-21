7d737650ef75142f36a39e906a574584be4620d1
Bob Yuan <bob.yuan@yahoo.com>
2023-11-20 16:11:20 -0800

    Add more UI elements on Manage home

--- src/app/manage/home/home.component.ts
+++ src/app/manage/home/home.component.ts
@@ -2,18 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    MatButtonModule,
     MatIconModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  openAssistant = false;
+
+  assitOpenClose() {
+    this.openAssistant = !this.openAssistant;
+  }
 
 }
