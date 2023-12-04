5ff0dcae9e4abf2db5d5e2a07de6dd76863b4739
Bob Yuan <bob.yuan@yahoo.com>
2023-12-04 13:10:18 -0800

    Add bottomsheet for AI Chat

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { AssistantComponent } from '../assistant/assistant.component';
 
 @Component({
   selector: 'app-footer',
@@ -8,10 +10,20 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule, 
     MatIconModule,
+    AssistantComponent,
+    MatBottomSheetModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  constructor(private _bottomSheet: MatBottomSheet) {}
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(AssistantComponent);
+  }
+
+
 
 }
